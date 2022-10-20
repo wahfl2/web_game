c2355cbeca1086a5c46e888ea68c11e2c4d88595
wahfl2 <wahflbeljihn@gmail.com>
2022-10-20 17:49:43 -0500

    Remove scaling, add sprites to player

--- src/game/player/spawn.rs
+++ src/game/player/spawn.rs
@@ -5,52 +5,74 @@ use super::{controls::{STEP_LENGTH, MAX_WEB_LENGTH}, components::*};
 
 pub fn player_spawn(
     mut commands: Commands,
-) {
-    // Player scale, only necessary to create joints properly
-    let p = 20.0;
 
+    asset_server: Res<AssetServer>,
+) {
     let group = CollisionGroups::new(Group::from_bits_truncate(0b10), Group::ALL);
 
-    let body = commands.spawn_bundle(TransformBundle::default())
-        .insert_bundle((
-            Collider::capsule_y(1.0, 1.0),
-            RigidBody::Dynamic,
-            Friction::coefficient(0.1),
-            Restitution::coefficient(0.7),
-            group,
-        )).id();
+    let body = commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("black_capsule.png"),
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(40.0, 80.0)),
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    }).insert_bundle((
+        Collider::capsule_y(20.0, 20.0),
+        RigidBody::Dynamic,
+        Friction::coefficient(0.1),
+        Restitution::coefficient(0.7),
+        group,
+    )).id();
 
     let joint = RevoluteJointBuilder::new()
-        .local_anchor1(Vec2::new(1.25 * p, p))
-        .local_anchor2(Vec2::new(-p, 0.0));
+        .local_anchor1(Vec2::new(25.0, 20.0))
+        .local_anchor2(Vec2::new(-20.0, 0.0));
 
-    let arm_l = commands.spawn_bundle(TransformBundle::default())
-        .insert_bundle((
-            Collider::capsule_x(0.75, 0.25),
-            RigidBody::Dynamic,
-            Damping { angular_damping: 5.0, linear_damping: 0.0 },
-            Friction::coefficient(0.5),
-            Restitution::coefficient(0.4),
-            ImpulseJoint::new(body, joint),
-            group,
-        )).id();
+    let arm_l = commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("arm_capsule.png"),
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(40.0, 10.0)),
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    }).insert_bundle((
+        Collider::capsule_x(15.0, 5.0),
+        RigidBody::Dynamic,
+        Damping { angular_damping: 5.0, linear_damping: 0.0 },
+        Friction::coefficient(0.5),
+        Restitution::coefficient(0.4),
+        ImpulseJoint::new(body, joint),
+        group,
+    )).id();
 
     let joint = RevoluteJointBuilder::new()
-        .local_anchor1(Vec2::new(-1.25 * p, p))
-        .local_anchor2(Vec2::new(p, 0.0));
+        .local_anchor1(Vec2::new(-25.0, 20.0))
+        .local_anchor2(Vec2::new(20.0, 0.0));
 
-    let arm_r = commands.spawn_bundle(TransformBundle::default())
-        .insert_bundle((
-            Collider::capsule_x(0.75, 0.25),
-            RigidBody::Dynamic,
-            Damping { angular_damping: 5.0, linear_damping: 0.0 },
-            Friction::coefficient(0.5),
-            Restitution::coefficient(0.4),
-            ImpulseJoint::new(body, joint),
-            group
-        )).id();
+    let arm_r = commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("arm_capsule.png"),
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(40.0, 10.0)),
+            flip_x: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    }).insert_bundle((
+        Collider::capsule_x(15.0, 5.0),
+        RigidBody::Dynamic,
+        Damping { angular_damping: 5.0, linear_damping: 0.0 },
+        Friction::coefficient(0.5),
+        Restitution::coefficient(0.4),
+        ImpulseJoint::new(body, joint),
+        group
+    )).id();
 
-    commands.spawn_bundle(TransformBundle::from_transform(Transform::from_scale(Vec3::splat(p))))
+    commands.spawn_bundle(TransformBundle::default())
+        .insert_bundle(VisibilityBundle::default())
         .insert(Player { body, arm_r, arm_l, attached: None })
         .insert_children(0, &[body, arm_r, arm_l]);
 }
