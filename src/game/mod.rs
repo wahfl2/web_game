46287bf29e906198aef75cd8b34ef6fc8e1b21ae
wahfl2 <wahflbeljihn@gmail.com>
2022-10-24 21:13:40 -0500

    Implemented player respawning in editor

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 use crate::editor::serde::editor_load;
 
-use self::player::{spawn::{player_spawn, preprocess_webs}, controls::player_controls, camera::player_camera, components::*};
+use self::player::{spawn::{player_spawn, preprocess_webs, Respawn}, controls::player_controls, camera::player_camera, components::*};
 
 pub mod level;
 pub mod player;
@@ -11,8 +11,9 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_system(editor_load)
-            .add_startup_system(player_spawn)
-            .add_startup_system(preprocess_webs)
+            .insert_resource(Respawn(true))
+            .add_system(player_spawn)
+            // .add_startup_system(preprocess_webs)
             .insert_resource(FailedShot(false))
             .add_system(player_camera)
             .add_system(player_controls);
