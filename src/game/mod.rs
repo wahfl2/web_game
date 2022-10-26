8f4c6a757b1dde89780a2949ac4c04ccf1a02b13
wahfl2 <wahflbeljihn@gmail.com>
2022-10-25 21:01:24 -0500

    Remove unused

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 use crate::editor::serde::editor_load;
 
-use self::player::{spawn::{player_spawn, preprocess_webs, Respawn}, controls::player_controls, camera::player_camera, components::*};
+use self::player::{spawn::{player_spawn, Respawn}, controls::player_controls, camera::player_camera, components::*};
 
 pub mod level;
 pub mod player;
@@ -13,7 +13,6 @@ impl Plugin for GamePlugin {
         app.add_system(editor_load)
             .insert_resource(Respawn(true))
             .add_system(player_spawn)
-            // .add_startup_system(preprocess_webs)
             .insert_resource(FailedShot(false))
             .add_system(player_camera)
             .add_system(player_controls);
